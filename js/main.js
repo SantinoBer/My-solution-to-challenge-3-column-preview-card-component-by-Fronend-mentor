f5d522dab1096be797a0b89433cd6f6747b86973
Santino <bertottisantino42gmail.com>
2022-10-12 12:09:52 -0300

    challenge solved + js added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//guarda el estado (columna activa)
+let columnaActiva = 1;
+
+//selecciona las columnas
+const columnas = document.querySelectorAll(".columna");
+
+//escuchar los "clicks" en cada una de ellas
+columnas.forEach((columna, indice) => { 
+    columna.addEventListener("click", function() {
+        cambiarColumna(indice);
+    })
+})
+
+//cambiar el estado de las columnas
+function cambiarColumna (indice) {
+    columnas[columnaActiva].classList.remove("activa");
+    columnas[indice].classList.add("activa");
+    columnaActiva = indice;
+}
